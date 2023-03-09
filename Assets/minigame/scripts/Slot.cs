03a28d05bdcb6242c056a64e9812de1743683b5d
Changheng Zhao <arthur2000@g.ucla.edu>
2023-03-09 12:50:30 -0800

    Fixed Slot.cs to report and return when the slot has >= 4 children (not > 4). The desired behavior of rejecting extra items to land on full slot should be implemented later.

--- Assets/minigame/scripts/Slot.cs
+++ Assets/minigame/scripts/Slot.cs
@@ -7,8 +7,11 @@ public class Slot : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
-        if (transform.childCount > 4) return;
         GameObject dropped = eventData.pointerDrag;
+        if (transform.childCount >= 4){
+            Debug.Log("More than 4 items in this slot.");
+            return;
+        }
         DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
         draggableItem.parentAfterDrag = transform;
         Debug.Log("slot assigned parentAfterDrag");
