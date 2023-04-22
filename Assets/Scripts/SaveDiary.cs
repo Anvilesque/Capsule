97ee6e654bd44ec5190a64c99fd6a126266b4354
Daniel Kao <dack8@outlook.com>
2023-04-22 16:16:24 -0700

    update diary to save entries on left page

--- Assets/Scripts/SaveDiary.cs
+++ Assets/Scripts/SaveDiary.cs
@@ -12,7 +12,7 @@ public class SaveDiary : MonoBehaviour
     public TaskManager taskManager;
     public TimeController time;
     public List<string> previousDiaryEntries = new List<string>();
-    public List<string> previousDiaryEntriesAlt = new List<string>();
+    public GameObject prevDiaryText;
            
     void Start()
     {
@@ -23,14 +23,24 @@ public class SaveDiary : MonoBehaviour
     public void Save()
     {
         string text = inputField.GetComponent<TMP_InputField>().text;
+        // no text to save, so return
+        if (text == "")
+            return;
         inputField.GetComponent<TMP_InputField>().text = "";
         taskManager.StopTask();
 
-        text = $"({System.DateTime.Now.Month}/{System.DateTime.Now.Day}/{System.DateTime.Now.Year})Day 1- {time.hours.ToString("00")}:{time.mins.ToString("00")}: {text}"; 
+        //$ is for putting in variables, @ is for breaking into multiple lines
+        text = $@"{System.DateTime.Now.Month}/{System.DateTime.Now.Day}/{System.DateTime.Now.Year}
+        |Day 1- {time.hours.ToString("00")}:{time.mins.ToString("00")}: {text}";
         previousDiaryEntries.Add(text);
+        //write new entries into left side.
+        string newText = "";
         foreach(var entry in previousDiaryEntries)
         {
-            Debug.Log(entry);
+            //only take the part after | for now
+            string[] parts = entry.Split('|');
+            newText += $"\n {parts[1]}";
         }
+        prevDiaryText.GetComponent<TMP_Text>().SetText(newText);
     }
 }
