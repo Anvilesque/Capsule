41970075012c04f79cb8f508a28f745625accfbb
megys <meganyschmid@gmail.com>
2023-05-10 21:06:21 -0700

    started cleaning minigame

---
+++ Assets/minigame/toolSlot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class toolSlot : MonoBehaviour
+{
+        public void OnItemSelected()
+        {
+            Debug.Log("hi");
+        }
+}
