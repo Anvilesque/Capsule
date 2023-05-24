3ca8c6cdd34373678029fa0e725f90fe12f9ab88
Helen Feng <53279829+hailenwashere@users.noreply.github.com>
2023-05-23 18:48:59 -0700

    Update DirtSpot.cs
    implemented dirt opacity lowering + set object active status to false when dirt is erased

--- Assets/minigame/DirtSpot.cs
+++ Assets/minigame/DirtSpot.cs
@@ -6,8 +6,24 @@ public class DirtSpot : MonoBehaviour
 {
     [SerializeField]
     public GameObject dirt;
-    [SerializeField]
+    
     public GameObject tool;
+    public SpriteRenderer dirtSprite;
+    float opacity;
+
+    void Start()
+    {
+        opacity = 1;
+        dirtSprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        if (opacity <= 0)
+        {
+            // disable object
+            dirt.SetActive(false);
+        }
+    }
 
     void OnMouseEnter()
     {
@@ -15,9 +31,16 @@ public class DirtSpot : MonoBehaviour
         Debug.Log($"this is {dirt.name}");
 
         if (!tool.activeSelf) // tool thumbnail selected = inactive image
+        {
             Debug.Log($"you have selected {tool.name}");
-        if (tool.activeSelf) // tool thumbnail unselected = active image
+            if (opacity > 0)
+            {
+                opacity -= .2f;
+            }
+        } else if (tool.activeSelf) // tool thumbnail unselected = active image
             Debug.Log($"you have not selected {tool.name}");
-    }   
+
+        dirtSprite.color = new Color(1f,1f,1f,opacity);
+    }
 
 }
