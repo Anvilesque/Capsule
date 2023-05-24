4d6babd011c103311e8af25a6d6b2431a005a35a
megys <meganyschmid@gmail.com>
2023-05-23 18:40:59 -0700

    sponge category needed for dirt

--- Assets/minigame/DirtSpot.cs
+++ Assets/minigame/DirtSpot.cs
@@ -6,11 +6,18 @@ public class DirtSpot : MonoBehaviour
 {
     [SerializeField]
     public GameObject dirt;
+    [SerializeField]
+    public GameObject tool;
 
     void OnMouseEnter()
     {
         //If your mouse hovers over the GameObject with the script attached, output this message
         Debug.Log($"this is {dirt.name}");
-    }
+
+        if (!tool.activeSelf) // tool thumbnail selected = inactive image
+            Debug.Log($"you have selected {tool.name}");
+        if (tool.activeSelf) // tool thumbnail unselected = active image
+            Debug.Log($"you have not selected {tool.name}");
+    }   
 
 }
