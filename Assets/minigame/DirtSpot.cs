810686444be87694e94530612e9c6bbef0118425
megys <meganyschmid@gmail.com>
2023-05-23 18:16:11 -0700

    added dirt spots

--- Assets/minigame/DirtSpot.cs
+++ Assets/minigame/DirtSpot.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class ToolBar : MonoBehaviour
+public class DirtSpot : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
