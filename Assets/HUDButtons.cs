7c2552c90801eba977556598eede249664e69f9b
stam5450 <84207054+stam5450@users.noreply.github.com>
2023-05-24 23:00:28 -0700

    hud UI updated

---
+++ Assets/HUDButtons.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HUDButtons : MonoBehaviour
+{
+    UIDocument buttonDocument;
+    Button settingButton;
+
+    void OnEnable()
+    {
+        buttonDocument = GetComponent<UIDocument>();
+        if (buttonDocument == null)
+        {
+            Debug.LogError("No button document found.")
+        }
+
+        settingButton = buttonDocument.rootVisualElement.Q("settings-button") as Button;
+        settingButton.RegisterCallback<ClickEvent>(SettingButtonClick);
+    }
+
+    public void SettingButtonClick(ClickEvent evt)
+    {
+
+    }
+
+}
