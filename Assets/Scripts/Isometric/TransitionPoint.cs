0b3723e7211a2623bdecf5ff9503519536955863
Alvin <98379004+Anvilesque@users.noreply.github.com>
2023-05-26 13:22:58 -0700

    Remove errors
    They don't affect anything but can be confusing/scary

--- Assets/Scripts/Isometric/TransitionPoint.cs
+++ Assets/Scripts/Isometric/TransitionPoint.cs
@@ -19,6 +19,7 @@ public class TransitionPoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (Application.isEditor) return;
         tileManager = FindObjectOfType<TileManager>();
         transitionMap = tileManager.transitionMap;
         positionCell = transitionMap.WorldToCell(transform.position);
@@ -27,12 +28,13 @@ public class TransitionPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!Application.isEditor) return;
         // Doing this just for the Editor; would normally get floorMap through tileManager
         if (transitionMap == null)
         {
             transitionMap = new List<Tilemap>(FindObjectsOfType<Tilemap>()).Find((Tilemap tilemap) => tilemap.name.Contains("Transition"));
         }
         // Update position in the Editor, for easier editing
-        if (Application.isEditor) positionCell = transitionMap.WorldToCell(transform.position);
+        positionCell = transitionMap.WorldToCell(transform.position);
     }
 }
