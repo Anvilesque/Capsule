82214b9b9d6f67b2507f9e40190cac5e0c0206ac
Daniel Kao <dack8@outlook.com>
2023-05-30 19:33:19 -0700

    fix merge

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -9,38 +9,50 @@ public class NPCMovement : MonoBehaviour
     private Tilemap floorMap;
     private NonPC nonPC;
     private Vector3Int destination;
+    public List<Vector3Int> destinations;
+    private int destinationInd;
     private Vector3Int nearestNodeNPC;
     private Vector3Int nearestNodeDestination;
     private bool isMoving;
+    private List<Vector3Int> path;
 
     // Start is called before the first frame update
     void Start()
     {
+        isMoving = false;
         tileManager = FindObjectOfType<TileManager>();
         floorMap = tileManager.floorMap;
         nonPC = GetComponent<NonPC>();
+        path = FindClosestPath(nonPC.position, destination, tileManager.tilesStandable);
+        destinationInd = 0;
     }
 
     void Update()
     {
-        
-    }
-
-    public void MoveNPCToDest(Vector3Int destination)
-    {
-        this.destination = destination;
-        FindClosestPath(nonPC.position, destination, tileManager.tilesStandable);
+        destination = destinations[destinationInd];
+        if (path.Count == 0 && nonPC.position != destination)
+        {
+            path = FindClosestPath(nonPC.position, destination, tileManager.tilesStandable);
+        }
+        else if (!isMoving && path.Count > 0)
+        {
+            Vector3Int dir = path[0] - nonPC.position;
+            StartCoroutine(MoveOne(dir));
+        }
     }
 
     List<Vector3Int> FindClosestPath(Vector3Int startposition, Vector3Int destination, List<Vector3Int> standableList)
     {
+        if (startposition == destination)
+            return new List<Vector3Int>();
         Queue<(Vector3Int, List<Vector3Int>)> nodesToSearch = new Queue<(Vector3Int, List<Vector3Int>)>();
         HashSet<Vector3Int> standableSet = new HashSet<Vector3Int>(standableList);
         HashSet<Vector3Int> visited = new HashSet<Vector3Int>();
         nodesToSearch.Enqueue((startposition, new List<Vector3Int>()));
-        while(nodesToSearch.Count != 0)
+        visited.Add(startposition);
+        while (nodesToSearch.Count != 0)
         {
-            (Vector3Int pos, List<Vector3Int> path)  = nodesToSearch.Dequeue();
+            (Vector3Int pos, List<Vector3Int> path) = nodesToSearch.Dequeue();
             if (pos == destination)
             {
                 return path;
@@ -53,74 +65,74 @@ public class NPCMovement : MonoBehaviour
             {
                 List<Vector3Int> newpath = new List<Vector3Int>(path);
                 newpath.Add(up);
-                nodesToSearch.Enqueue((startposition, newpath));
+                nodesToSearch.Enqueue((up, newpath));
                 visited.Add(up);
             }
             if (standableSet.Contains(down) && !visited.Contains(down))
             {
                 List<Vector3Int> newpath = new List<Vector3Int>(path);
                 newpath.Add(down);
-                nodesToSearch.Enqueue((startposition, newpath));
+                nodesToSearch.Enqueue((down, newpath));
                 visited.Add(down);
             }
             if (standableSet.Contains(left) && !visited.Contains(left))
             {
                 List<Vector3Int> newpath = new List<Vector3Int>(path);
                 newpath.Add(left);
-                nodesToSearch.Enqueue((startposition, newpath));
+                nodesToSearch.Enqueue((left, newpath));
                 visited.Add(left);
             }
             if (standableSet.Contains(right) && !visited.Contains(right))
             {
                 List<Vector3Int> newpath = new List<Vector3Int>(path);
                 newpath.Add(right);
-                nodesToSearch.Enqueue((startposition, newpath));
+                nodesToSearch.Enqueue((right, newpath));
                 visited.Add(right);
             }
         }
         // not good if it exists without finding since this means that there is no path!
         return new List<Vector3Int>();
     }
+    IEnumerator MoveOne(Vector3Int direction)
+    {
+        float movementSpeed = 3f;
+        float timeToMove = 1 / movementSpeed;
+        if (timeToMove < 0) yield break;
 
-    // IEnumerator MoveOne(Vector3Int direction)
-    // {
-    //     float movementSpeed = 3f;
-    //     float timeToMove = 1 / movementSpeed;
-    //     if (timeToMove < 0) yield break;
+        FaceDirection(direction);
 
-    //     FaceDirection(direction);
+        Vector3Int prevPosition = nonPC.position;
+        Vector3Int tempPosition = prevPosition + direction;
 
-    //     Vector3Int prevPosition = nonPC.position;
-    //     Vector3Int tempPosition = prevPosition + direction;
+        // Check if next position is standable
+        if (tileManager.tilesStandable.Contains(tempPosition)) { }
+        else if (tileManager.tilesStandable.Contains(new Vector3Int(tempPosition.x, tempPosition.y, tempPosition.z - 2)))
+        {
+            tempPosition.z -= 2;
+        }
+        else if (tileManager.tilesStandable.Contains(new Vector3Int(tempPosition.x, tempPosition.y, tempPosition.z + 2)))
+        {
+            tempPosition.z += 2;
+        }
+        else yield break;
 
-    //     // Check if next position is standable
-    //     if (tileManager.tilesStandable.Contains(tempPosition)) {}
-    //     else if (tileManager.tilesStandable.Contains(new Vector3Int(tempPosition.x, tempPosition.y, tempPosition.z - 2)))
-    //     {
-    //         tempPosition.z -= 2;
-    //     }
-    //     else if (tileManager.tilesStandable.Contains(new Vector3Int(tempPosition.x, tempPosition.y, tempPosition.z + 2)))
-    //     {
-    //         tempPosition.z += 2;
-    //     }
-    //     else yield break;
+        // Check if player is in the way
+        if (FindObjectOfType<PlayerMovement>().transform.position == tempPosition) yield break;
 
-    //     // Check if NPC is in the way
-    //     if (GetComponent<PlayerNPCEncounter>().GetNPCAtPosition(tempPosition) != null) yield break;
-
-    //     // All checks cleared --> handle movement
-    //     isMoving = true;
-    //     float elapsedTime = 0f;
-    //     while(elapsedTime < timeToMove)
-    //     {
-    //         // Lerp moves from one position to the other in some amount of time.
-    //         transform.position = Vector3.Lerp(floorMap.CellToWorld(prevPosition), floorMap.CellToWorld(tempPosition), (elapsedTime / timeToMove));
-    //         elapsedTime += Time.deltaTime;
-    //         yield return null;
-    //     }
-    //     transform.position = floorMap.CellToWorld(tempPosition);
-    //     isMoving = false;
-    // }
+        // All checks cleared --> handle movement
+        isMoving = true;
+        float elapsedTime = 0f;
+        while (elapsedTime < timeToMove)
+        {
+            // Lerp moves from one position to the other in some amount of time.
+            transform.position = Vector3.Lerp(floorMap.CellToWorld(prevPosition), floorMap.CellToWorld(tempPosition), (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = floorMap.CellToWorld(tempPosition);
+        path.RemoveAt(0);
+        isMoving = false;
+    }
 
     void FaceDirection(Vector3Int direction)
     {
