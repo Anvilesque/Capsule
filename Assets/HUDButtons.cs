ce50a3e040cb11454fd2260f17ed0f99f91c4bd5
stam5450 <84207054+stam5450@users.noreply.github.com>
2023-06-01 23:39:59 -0700

    updated hud script

--- Assets/HUDButtons.cs
+++ Assets/HUDButtons.cs
@@ -2,20 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using DG.Tweening;
 
 public class HUDButtons : MonoBehaviour
 {
+    private VisualElement root;
     UIDocument buttonDocument;
     Button settingButton;
+
+    private Label timeTime;
+    private Label timeDay;
+    private TimeController timeController;
+
+    void Start()
+    {
+        root = GetComponent<UIDocument>().rootVisualElement;
+        timeController = FindObjectOfType<TimeController>();
+        timeTime = root.Q<Label>("timeTime");
+        timeDay = root.Q<Label>("timeDay");
+        timeTime.text = timeController.timeTextTime;
+        timeDay.text = timeController.timeTextDay;
+    }
+
+    private void Update()
+    {
+        timeTime.text = timeController.timeTextTime;
+        timeDay.text = timeController.timeTextDay;
+    }
 
     void OnEnable()
     {
         buttonDocument = GetComponent<UIDocument>();
+<<<<<<< Updated upstream
         if (buttonDocument == null)
         {
             Debug.LogError("No button document found.");
         }
 
+=======
+>>>>>>> Stashed changes
         settingButton = buttonDocument.rootVisualElement.Q("settings-button") as Button;
         settingButton.RegisterCallback<ClickEvent>(SettingButtonClick);
     }
