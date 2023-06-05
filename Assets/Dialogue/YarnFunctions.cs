4cf495d35aa4a09eaede4cba004b564b3bbf8204
Alvin <98379004+Anvilesque@users.noreply.github.com>
2023-06-05 10:13:50 -0700

    Set up some dialogue

--- Assets/Dialogue/YarnFunctions.cs
+++ Assets/Dialogue/YarnFunctions.cs
@@ -6,10 +6,32 @@ using Yarn.Unity;
 public class YarnFunctions : MonoBehaviour
 {
     public List<string> nodesBen;
+    private TimeController timeController;
+    private DialogueRunner dialogueRunner;
+    private Dictionary<string, int> npcToNumber;
+    private List<string> npcs;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<DialogueRunner>().AddFunction("GetRandomDialogue", (int numDialogue) => {return GetRandomDialogue(numDialogue);} );
+        timeController = FindObjectOfType<TimeController>();
+        dialogueRunner = GetComponent<DialogueRunner>();
+        
+        npcs = new List<string>() {"ben", "aldrich", "winter", "joanne"};
+        npcToNumber = new Dictionary<string, int>();
+        ResetNPCDialogueNumber();
+
+        dialogueRunner.AddFunction("GetRandomDialogue", (int numDialogue) => {return GetRandomDialogue(numDialogue);} );
+        dialogueRunner.AddFunction("GetDay", () => {return GetDay();} );
+        dialogueRunner.AddFunction("GetNPCDialogueNumber", (string npc) => {return GetNPCDialogueNumber(npc);} );
+    }
+    
+    public void ResetNPCDialogueNumber()
+    {
+        foreach (string npc in npcs)
+        {
+            npcToNumber[npc] = 1;
+        }
     }
 
     public int GetRandomDialogue(int numDialogue)
@@ -17,9 +39,19 @@ public class YarnFunctions : MonoBehaviour
         return Random.Range(0, numDialogue);
     }
 
-    // Update is called once per frame
-    void Update()
+    public int GetDay()
     {
-        
+        return timeController.days;
+    }
+
+    public int GetNPCDialogueNumber(string npc)
+    {
+        return npcToNumber[npc];
+    }
+
+    [YarnCommand("setNextDialogueFor")]
+    public void IncreaseNPCDialogueNumber(string npc)
+    {
+        npcToNumber[npc]++;
     }
 }
